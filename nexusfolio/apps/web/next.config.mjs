985896efd7e2fa6e2963d07a41ce088908af91af
Roshan Iruku <roshaniruku@Roshans-MacBook-Air.local>
2025-10-04 10:13:55 -0400

    working manual stuff

--- nexusfolio/apps/web/next.config.mjs
+++ nexusfolio/apps/web/next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.logo.dev',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'logo.clearbit.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
